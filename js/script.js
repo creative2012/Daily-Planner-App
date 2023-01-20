3631777e9f0bf34db2277c27243560e50c44c286
Paul Morris <creative2012@hotmail.com>
2023-01-20 18:57:22 +0000

    added logic to check and save to local storage

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@ function generateTimeBlocks() {
         timeBlock.addClass('timeBlock');
         hour.addClass('hour');
         textArea.addClass(getHourClass(i));
+        textArea.attr('data-time', time);
         saveBtn.addClass('saveBtn clickEvent');
         icon.addClass('fa fa-cloud-upload-alt clickEvent');
         //append to page
@@ -48,32 +49,65 @@ function generateTimeBlocks() {
         
         time < 12 ? time++ : time = 1;
     }
+    //populate fields if local storage data available
+    checkLocalStorage('populate');
 
 }
 //function to save calender event
-function saveToCalender(){
+function saveToCalender(timeSlot, entry){
+    let event = {
+        time: timeSlot,
+        entry: entry
+    };
+    //check if data in local storage and return it if so
+    let data = checkLocalStorage('save');
+    if(data != null){
+        //add new event to already stored data
+        data.push(event);
+    } else {
+        //or just add new event
+        data = event;
+    }
+    //save to local storage
+    localStorage.setItem('calender', JSON.stringify(data));
 
 }
 //function to check local storage data
-function checkLocalStorage(){
+function checkLocalStorage(type){
     //check if calender data available for current day
     let data = JSON.parse(localStorage.getItem('calender'));
-    if (data != null) {
-        if(data.date != currentDate){
-            data = null;
-            localStorage.removeItem('calender');
+    //logic to populate from data
+    if(type == "populate"){
+        if (data != null) {
+            //if not same day, remove key 
+            if(data.date != currentDate){
+                localStorage.removeItem('calender');
+                return;
+            }
+            //if found, populate 
+            populateFromLocalStorage(data);
+            return;
         }
     }
-    console.log(data);
-    return data;
+    //logic to check if localStorage data avaialble
+    if(type == "save"){
+        //if data available add to array and return
+        if (data != null) {
+            let events = [];
+            data.forEach(function (item) {
+                toStorage.push(item);
+            });
+            return events;
+        }
+        //if no data in local storage
+        return null;
+    }
+    
 
 }
 //function to retrive calender event from localStorage
-function getFromLocalStorage(){
+function populateFromLocalStorage(){
 
-    if(checkLocalStorage() != null){
-
-    }
 
 }
 //Set header Current Day
@@ -83,7 +117,12 @@ generateTimeBlocks();
 checkLocalStorage();
 //listen for request to save
 timeBlockCont.on('click',function(e){
+    
     if(e.target.classList.contains('clickEvent')){
-        saveToCalender();
+        let target = $(this).parent();
+        console.log(target);
+        let timeSlot = target.childern('data-time').value;
+        let entry = target.childern('.event-area').value;
+        saveToCalender(timeSlot, entry);
     }
 })
