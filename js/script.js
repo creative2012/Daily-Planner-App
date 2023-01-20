bde014e0c642ec2cfe3bac180070693a5f0de67b
Paul Morris <creative2012@hotmail.com>
2023-01-20 18:19:28 +0000

    changed hours available to just working hours & changed associated function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 const currentDay = $('#currentDay');
 const timeBlockCont = $('.SchedContainer');
+var localStorageData = '';
+const currentDate = moment().format('dddd, Do MMMM YYYY');
+const current = moment().hours();
 
-//function to check which class and time prefix to add
-function getHour(func, i) {
+//function to check which class to add
+function getHourClass(i) {
 
     //check class for text input area
-    if (func == 'class') {
-        let current = moment().hours();
         let className = 'event-area';
 
         if (i < current) {
@@ -18,23 +19,13 @@ function getHour(func, i) {
         }
         return className;
 
-    }
-    //check prefix for am/pm
-    if (func == 'prefix') {
-        let hourText = 'AM';
-        if (i > 11 && i != 24) {
-            hourText = 'PM';
-        }
-        return hourText;
-    }
-
 }
 //Function to generate time blocks
 function generateTimeBlocks() {
 
-    let time = 12;
+    let time = 9;
 
-    for (var i = 0; i < 24; i++) {
+    for (var i = 0; i < 9; i++) {
 
         //create the Elements
         let timeBlock = $('<div>');
@@ -45,11 +36,12 @@ function generateTimeBlocks() {
         //Add classes
         timeBlock.addClass('timeBlock');
         hour.addClass('hour');
-        textArea.addClass(getHour('class', i));
+        textArea.addClass(getHourClass(i));
         saveBtn.addClass('saveBtn clickEvent');
         icon.addClass('fa fa-cloud-upload-alt clickEvent');
         //append to page
-        hour.text(time + getHour('prefix', i))
+        let hourText = i < 3 ? 'AM' : 'PM';
+        hour.text(time + hourText)
         saveBtn.append(icon);
         timeBlock.append(hour).append(textArea).append(saveBtn);
         timeBlockCont.append(timeBlock);
@@ -58,13 +50,40 @@ function generateTimeBlocks() {
     }
 
 }
+//function to save calender event
+function saveToCalender(){
+
+}
+//function to check local storage data
+function checkLocalStorage(){
+    //check if calender data available for current day
+    let data = JSON.parse(localStorage.getItem('calender'));
+    if (data != null) {
+        if(data.date != currentDate){
+            data = null;
+            localStorage.removeItem('calender');
+        }
+    }
+    console.log(data);
+    return data;
+
+}
+//function to retrive calender event from localStorage
+function getFromLocalStorage(){
+
+    if(checkLocalStorage() != null){
+
+    }
+
+}
 //Set header Current Day
-currentDay.text(moment().format('dddd, Do YYYY'));
+currentDay.text(currentDate);
 //Generate TimeBlocks
 generateTimeBlocks();
-
+checkLocalStorage();
+//listen for request to save
 timeBlockCont.on('click',function(e){
     if(e.target.classList.contains('clickEvent')){
-        console.log('yes')
+        saveToCalender();
     }
 })
